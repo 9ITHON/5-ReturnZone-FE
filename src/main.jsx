5a295b48e9a0548318d4bdb7b7df5b548bca0f66
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-06-29 17:45:02 +0900

    commit 오류수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -18,6 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode className="app-viewport">
     <App  />
-  </StrictMode>,
+  </StrictMode>
 )
->>>>>>> 13cf750 (input-field 작성)
