1ac12a3142613c1f7ba7106886d87fd2a376b22f
SUNG_HWAN <43512783+kim10914@users.noreply.github.com>
2025-06-29 16:24:55 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import './App.css'
-
-// 메인 페이지
-function App() {
-
-  return (
-    <div>메인 페이지 입니다.</div>
-  )
-}
-
-export default App
-=======
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'; // Router 추가
 import SignUp from './pages/signup';
 import MainPage from './pages/main-page'; //임시 메인 페이지
@@ -37,4 +24,3 @@ function App() {
 }
 
 export default App
->>>>>>> 13cf750 (input-field 작성)
