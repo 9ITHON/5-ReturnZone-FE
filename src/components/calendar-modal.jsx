6296085fa8fefe0be304f260d71538db87297c2a
SUNG_HWAN <43512783+kim10914@users.noreply.github.com>
2025-07-08 20:14:05 +0900

    Update calendar-modal.jsx
    모달 외부 클릭 업데이트

--- src/components/calendar-modal.jsx
+++ src/components/calendar-modal.jsx
@@ -1,32 +1,37 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import { enUS } from "date-fns/locale";
 import { format } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import Button from "./button";
 
-export default function CalendarModal({ onClose, onSelect }) {
+const CalendarModal = forwardRef(({ onClose, onSelect }, ref) => {
     const [selected, setSelected] = useState();
 
     const handleSelect = () => {
         if (selected) {
             onSelect(selected); // 선택된 날짜 전달
-            onClose();           // 모달 닫기
+            onClose();          // 모달 닫기
         }
     };
 
-
     return (
-        <div className="fixed inset-0 z-50 flex justify-center items-end bg-[#11111180] ">
-            <div className="bg-white rounded-t-[20px] w-full max-w-md px-[24px] pt-[20px] pb-[32px]">
-                <h2 className="text-[16px] text-[#111111] font-semibold text-center py-[11px] px-[24px]">날짜 선택</h2>
+        <div className="fixed inset-0 z-50 flex justify-center items-end bg-[#11111180]">
+            <div
+                ref={ref}
+                className="bg-white rounded-t-[20px] w-full max-w-md px-[24px] pt-[20px] pb-[32px]"
+            >
+                <h2 className="text-[16px] text-[#111111] font-semibold text-center py-[11px] px-[24px]">
+                    날짜 선택
+                </h2>
                 <DayPicker
                     mode="single"
                     selected={selected}
                     onSelect={setSelected}
                     locale={enUS}
                     formatters={{
-                        weekdayName: (date) => format(date, "EEE", { locale: enUS }) // 'EEE' = 'Mon', 'Tue' 왜 안돼...
+                        weekdayName: (date) =>
+                            format(date, "EEE", { locale: enUS }),
                     }}
                     className="ml-[10px]"
                 />
@@ -36,4 +41,6 @@ export default function CalendarModal({ onClose, onSelect }) {
             </div>
         </div>
     );
-}
+});
+
+export default CalendarModal;
