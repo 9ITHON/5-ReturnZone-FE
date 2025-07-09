eabf410df8bb20e48b52138d0ad07bdd14a2c59e
leeseoleeseoyeon <seoyeon6468@gmail.com>
2025-07-09 17:12:05 +0900

    fix : 스크롤바 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -146,7 +146,7 @@ const Home = () => {
   }
 
   return (
-    <div className="relative w-[390px] h-[844px] bg-white flex flex-col items-center mx-auto overflow-hidden">
+    <div className="relative w-[390px] h-screen bg-white flex flex-col items-center mx-auto overflow-hidden">
       <MainHeader />
       {/* 상단 바: 필터바 + 검색 아이콘 */}
       {!categoryOpen && (
