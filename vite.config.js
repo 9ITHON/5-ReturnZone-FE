df9715573d0607f0a32d80da86ed5994349a2fb1
leeseoleeseoyeon <seoyeon6468@gmail.com>
2025-07-12 14:40:19 +0900

    fix : 로그인 가능하게 해주세요

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],	
-  server: {	
+    plugins: [react(), tailwindcss(), netlifyPlugin()],
+  server: {
     proxy: {	
       '/api': {	
         target: 'https://15.164.234.32.nip.io',	
