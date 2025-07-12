2b022ceb7d5425e579eea26854cc7346c12d7141
leeseoleeseoyeon <seoyeon6468@gmail.com>
2025-07-12 12:41:09 +0900

    fix : 이건 아닌듯

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// 카카오 로그인 콜백 URL 접근 시 강제 리다이렉트 (임시 조치)
-if (window.location.pathname.startsWith('/auth/login/kakao')) {
-  window.location.replace('/Home');
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="app-viewport">
